1ef949674693fb74e5dd6e4090d0641134ad1709
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-02-10 20:35:58 +0530

    [vidyadhar hudge]Add:validation for Reset' page

--- src/components/pages/ResetPassword/ResetPassword.jsx
+++ src/components/pages/ResetPassword/ResetPassword.jsx
@@ -10,51 +10,81 @@ export default class ResetPassword extends Component {
     super(props);
     this.state = {
       Email: '',
-      Password:'',
-      ConfirmPassword:''
-  }
+      Password: '',
+      ConfirmPassword: '',
+      EmailError: '',
+      PasswordError: '',
+      ConfirmPasswordError: '',
+      MatchingPasswordError: ''
+    }
   }
 
   emailsHandlers = (event) => {
     this.setState({
-      Email: event.target.value
-  })
-  console.log("email",this.state.Email)
+      Email: event.target.value,
+      EmailError: ''
+    })
+    console.log("email", this.state.Email)
   };
-  
+
   resertpasswordHandlers = (event) => {
-  this.setState({
-    Password: event.target.value
-  })
-  console.log("password",this.state.Password)
+    this.setState({
+      Password: event.target.value,
+      PasswordError: ''
+    })
+    console.log("password", this.state.Password)
   };
 
   confirmPasswordHandlers = (event) => {
     this.setState({
-      ConfirmPassword: event.target.value
+      ConfirmPassword: event.target.value,
+      ConfirmPasswordError: ''
     })
-    console.log("password",this.state.ConfirmPassword)
-    };
+    console.log("password", this.state.ConfirmPassword)
+  };
 
   handleReset = () => {
-    console.log("dhirajs");
-    const resetData ={
-      email:this.state.Email,
-      password:this.state.Password,
-      confirmPassword:this.state.ConfirmPassword,
+    if (!this.state.Email.match("^[a-zA-Z0-9]{1,}([.]?[-]?[+]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2})?$")) {
+      this.setState({
+        EmailError: "Email is not valid"
+      })
     }
-    
-    reset(resetData).then((responce)=>{
-    if(responce.data.status === true){
-      console.log("Reset Password successful!")
-        this.props.history.push("\login")
+    if (!this.state.Password.match("[A-Za-z0-9!@#$%^&*()_]{6,20}")) {
+      this.setState({
+        PasswordError: "Password is not valid"
+      })
     }
-      console.log("responce data==>",responce);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  }
+    if (!this.state.ConfirmPassword.match("[A-Za-z0-9!@#$%^&*()_]{6,20}")) {
+      this.setState({
+        ConfirmPasswordError: "Confirm Password is not valid"
+      })
+    }
+    if (this.state.ConfirmPassword != this.state.Password) {
+      this.setState({
+        MatchingPasswordError: "Password and confirm password not same"
+      })
+    }
+    if ((this.state.Email.match("^[a-zA-Z0-9]{1,}([.]?[-]?[+]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2})?$"))
+      && (this.state.Password.match("[A-Za-z0-9!@#$%^&*()_]{6,20}")) &&
+      (this.state.ConfirmPassword.match("[A-Za-z0-9!@#$%^&*()_]{6,20}"))
+      && (this.state.ConfirmPassword === this.state.Password)) {
+      const resetData = {
+        email: this.state.Email,
+        password: this.state.Password,
+        confirmPassword: this.state.ConfirmPassword,
+      }
 
+      reset(resetData).then((responce) => {
+        if (responce.data.status === true) {
+          console.log("Reset Password successful!")
+          this.props.history.push("\login")
+        }
+        console.log("responce data==>", responce);
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  }
   render() {
     return (
       <div className="Reset">
@@ -68,10 +98,10 @@ export default class ResetPassword extends Component {
             <div className="red">o</div>
           </div>
           <div >
-            <TextField id="emailReset" label="Email Id" variant="outlined" onChange={this.emailsHandlers} value={this.state.email}/>
+            <TextField id="emailReset" label="Email Id" variant="outlined" onChange={this.emailsHandlers} value={this.state.email} error={this.state.EmailError} helperText={this.state.EmailError} />
           </div>
           <div>
-            <TextField id="passwordReset" label="Password" variant="outlined" onChange={this.resertpasswordHandlers} value={this.state.password} {...this.props}
+            <TextField id="passwordReset" label="Password" variant="outlined" onChange={this.resertpasswordHandlers} value={this.state.password} value={this.state.password} error={((this.state.PasswordError) || (this.state.MatchingPasswordError))} helperText={((this.state.PasswordError) || (this.state.MatchingPasswordError))} {...this.props}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
@@ -82,7 +112,7 @@ export default class ResetPassword extends Component {
             />
           </div>
           <div>
-            <TextField id="confirmPasswordReset" label="Confirm Password" variant="outlined" onChange={this.confirmPasswordHandlers} value={this.state.confirmPassword} {...this.props}
+            <TextField id="confirmPasswordReset" label="Confirm Password" variant="outlined" onChange={this.confirmPasswordHandlers} value={this.state.confirmPassword} value={this.state.confirmPassword} error={((this.state.ConfirmPasswordError) || (this.state.MatchingPasswordError))} helperText={((this.state.ConfirmPasswordError) || (this.state.MatchingPasswordError))}{...this.props}
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
