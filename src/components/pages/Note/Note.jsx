d572a0b880f9f972b8e6c2ae3a4f8f5f5c504a7b
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-02-15 14:42:18 +0530

    [vidyadhar hudge]Add:reminder,collaborator,colour pallet,image,trash and close button on card

--- src/components/pages/Note/Note.jsx
+++ src/components/pages/Note/Note.jsx
@@ -5,8 +5,14 @@ import '../Note/Notes.scss'
 import { TextField } from '@material-ui/core'
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
 import IconButton from '@material-ui/core/IconButton';
-import Pin from  "../../../assets/push.svg"
+import Pin from "../../../assets/push.svg"
 import Push from "../../../assets/Pin.svg"
+import AddAlertOutlinedIcon from '@material-ui/icons/AddAlertOutlined';
+import { Tooltip } from "@material-ui/core";
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
+import PaletteOutlinedIcon from '@material-ui/icons/PaletteOutlined';
+import { Button } from '@material-ui/core';
 
 export default function Notes() {
 
@@ -31,43 +37,69 @@ export default function Notes() {
 
     const [pin, unpin] = React.useState(false);
     const pins = () => {
-      unpin(!pin);
-      console.log("pin ",pin)
+        unpin(!pin);
+        console.log("pin ", pin)
     }
     return (
         <ClickAwayListener onClickAway={onHandleClick}>
             <div className="mainCard">
-                {card ?<Card id="active" >
-                        <div className="textFieldContainer">
-                            <TextField
-                                className="textfield"
-                                fullWidth
-                                placeholder='Title'
-                                textdecaration='none'
-                                onChange={titleHandler}
-                                multiline
-                                InputProps={{ disableUnderline: true }}
-                               
-                            />
-                           {pin ? <img id="pin" src={Pin}  onClick={pins} />:<img id="unPin" src={Push}  onClick={pins}/>}
+                {card ? <Card id="active" >
+                    <div className="textFieldContainer">
+                        <TextField
+                            className="textfield"
+                            fullWidth
+                            placeholder='Title'
+                            textdecaration='none'
+                            onChange={titleHandler}
+                            multiline
+                            InputProps={{ disableUnderline: true }}
+
+                        />
+                        {pin ? <img id="pin" src={Pin} onClick={pins} /> : <img id="unPin" src={Push} onClick={pins} />}
                     </div>
-                        <div className="textFieldContainer">
-                            <TextField
-                                className="textfield"
-                                fullWidth
-                                placeholder='Takes Note...'
-                                textdecaration='none'
-                                onChange={NoteHandler}
-                                multiline
-                                InputProps={{ disableUnderline: true }}
-                            />
+                    <div className="textFieldContainer">
+                        <TextField
+                            className="textfield"
+                            fullWidth
+                            placeholder='Takes Note...'
+                            textdecaration='none'
+                            onChange={NoteHandler}
+                            multiline
+                            InputProps={{ disableUnderline: true }}
+                        />
+                    </div>
+                    <div className="icon">
+                        <div className="iconContainer">
+                            <Tooltip title="Reminde me">
+                                <div><IconButton > <AddAlertOutlinedIcon fontSize="small" /> </IconButton></div>
+                            </Tooltip>
+                            <Tooltip title="Colabrator">
+                                <div><IconButton > <PersonAddOutlinedIcon fontSize="small" /> </IconButton></div>
+                            </Tooltip>
+                            <Tooltip title="change Color">
+                                <div><IconButton > <PaletteOutlinedIcon closefontSize="small" /></IconButton></div>
+                            </Tooltip>
+                            <Tooltip title="image add">
+                                <div>
+                                    <IconButton >
+                                        <ImageOutlinedIcon
+                                            fontSize="small" /> </IconButton>
+                                </div>
+                            </Tooltip>
+                            <Tooltip title="Archive">
+                                <div><IconButton> <ArchiveOutlinedIcon fontSize="small" /> </IconButton></div>
+                            </Tooltip>
                         </div>
-                    </Card>
+                        <div>
+                            <Button id="button">close</Button>
+                        </div>
+                    </div>
+                </Card>
                     :
                     <Card id="inactive" onClick={inactiveNote}>
                         Take Note...
-                        <IconButton><ImageOutlinedIcon fontSize='medium' /> </IconButton>
-                     </Card>}
+                        <IconButton><ImageOutlinedIcon id="image" fontSize='medium' /> </IconButton>
+                    </Card>}
             </div>
         </ClickAwayListener>
     )
